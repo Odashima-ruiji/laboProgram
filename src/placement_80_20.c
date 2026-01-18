fad311d6021c5f49c5e293be72da688406b1bc27
odashima <t22b242a@mail.cc.niigata-u.ac.jp>
2026-01-19 02:24:34 +0900

    修正

--- src/placement_80_20.c
+++ src/placement_80_20.c
@@ -27,7 +27,7 @@ void Dist_Init_p(void)
  * --------------------------------------------------------- */
 void Dist_Init_n(void)
 {
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 5; i++)
     {
         do
         {
@@ -35,71 +35,35 @@ void Dist_Init_n(void)
             Node[i].n_Y = center_y - 2 * i;
         } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X > center_x || Node[i].n_Y > center_y));
     }
-    for (int i = 8; i < 10; i++)
+    
+    for (int i = 5; i < 10; i++)
     {
         do
         {
             Node[i].n_X = randAxAy();
-            Node[i].n_Y = randAxAy();
-        } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X > center_x || Node[i].n_Y > center_y));
-    }
-    for (int i = 10; i < 18; i++)
-    {
-        do
-        {
-            Node[i].n_X = randAxAy();
-            Node[i].n_Y = center_y + 2 * (i - 10) + 1;
+            Node[i].n_Y = center_y + 2 * (i - 5) + 1;
         } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X > center_x || Node[i].n_Y < center_y));
     }
-    for (int i = 18; i < 20; i++)
+    
+    for (int i = 10; i < 15; i++)
     {
         do
         {
             Node[i].n_X = randAxAy();
-            Node[i].n_Y = randAxAy();
-        } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X > center_x || Node[i].n_Y < center_y));
-    }
-    for (int i = 20; i < 28; i++)
-    {
-        do
-        {
-            Node[i].n_X = randAxAy();
-            Node[i].n_Y = center_y + 2 * (i - 20);
+            Node[i].n_Y = center_y + 2 * (i - 10);
         } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X < center_x || Node[i].n_Y < center_y));
     }
-    for (int i = 28; i < 30; i++)
+   
+    for (int i = 15; i < 20; i++)
     {
         do
         {
             Node[i].n_X = randAxAy();
-            Node[i].n_Y = randAxAy();
-        } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X < center_x || Node[i].n_Y < center_y));
-    }
-    for (int i = 30; i < 38; i++)
-    {
-        do
-        {
-            Node[i].n_X = randAxAy();
-            Node[i].n_Y = center_y - 2 * (i - 30) - 1;
+            Node[i].n_Y = center_y - 2 * (i - 15) - 1;
         } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X < center_x || Node[i].n_Y > center_y));
     }
-    for (int i = 38; i < 40; i++)
-    {
-        do
-        {
-            Node[i].n_X = randAxAy();
-            Node[i].n_Y = randAxAy();
-        } while (((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2)) || (Node[i].n_X < center_x || Node[i].n_Y > center_y));
-    }
-    // for(int i = 0; i < 10; i++)
-    // {
-    //     do
-    //     {
-    //         Node[i].n_X = randAxAy();
-    //         Node[i].n_Y = randAxAy();
-    //     } while ((sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) < area_disaster) || (sqrt2(Node[i].n_X - center_x, Node[i].n_Y - center_y) > area_disaster + 2));
-    // }
-    for (int i = 40; i < N_ALL_NUM; i++)
+    
+    for (int i = 20; i < N_ALL_NUM; i++)
     {
         do
         {
